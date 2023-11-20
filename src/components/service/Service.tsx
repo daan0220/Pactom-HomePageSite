ea91cd8b09a683e6fbfe74884d4c800e1545a80e
daan0220 <daikisoccer02@icloud.com>
2023-11-20 17:30:09 +0900

    eee

--- src/components/service/Service.tsx
+++ src/components/service/Service.tsx
@@ -3,6 +3,8 @@ import InitialTransition from "../../utils/InitialTransition";
 import Footer from "../Footer";
 import '@fortawesome/fontawesome-free/css/all.css'; 
 import ServiseImg from "../../Images/スクリーンショット 2023-11-16 11.32.23.png";
+import matchImg from "../../Images/スクリーンショット 2023-11-20 17.09.47.png";
+import DAOImg from "../../Images/スクリーンショット 2023-11-20 17.16.14.png";
 import { Link } from 'react-router-dom';
 import { Button } from "react-bootstrap";
 
@@ -18,12 +20,38 @@ const Contact = () => {
       >
         <section id="service">
           <div className="text-center">
+            <h1>Tech Match</h1>
+            <div className="row text-center">
+                <div className="col-md-6 offset-md-3">
+                <img src={matchImg} className="" alt="" />
+                  <h4 className="my-3"></h4>
+                  <Link to="https://github.com/daan0220/rails-ecsite">
+                    <Button variant="link">詳細はこちらをクリック</Button>
+                  </Link>
+                  <p className="text-muted">
+                    
+                  </p>
+                </div>
+              </div>
             <h1>ECommerce</h1>
             <div className="row text-center">
                 <div className="col-md-6 offset-md-3">
                 <img src={ServiseImg} className="" alt="" />
                   <h4 className="my-3"></h4>
                   <Link to="https://github.com/daan0220/rails-ecsite">
+                    <Button variant="link">詳細はこちらをクリック</Button>
+                  </Link>
+                  <p className="text-muted">
+                    
+                  </p>
+                </div>
+              </div>
+            <h1>J-leagueDAO</h1>
+            <div className="row text-center">
+                <div className="col-md-6 offset-md-3">
+                <img src={DAOImg} className="" alt="" />
+                  <h4 className="my-3"></h4>
+                  <Link to="https://github.com/shake551/fan-chain">
                     <Button variant="link">詳細はこちらをクリック</Button>
                   </Link>
                   <p className="text-muted">
